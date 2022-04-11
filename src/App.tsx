698909c064b94cb05cfe63f22723bc2982cb17f6
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-11 17:47:49 +0300

    feat: set up react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+  return (
+      <>
+        <h1>Instagram clone with</h1>
+        <h4>React 18 and TypeScript 4</h4>
+      </>
+  )
+}
+
+export default App
