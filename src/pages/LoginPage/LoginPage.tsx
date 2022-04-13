bbeafa13bf6aebe083148d9667470df1c4ca396f
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-13 18:16:54 +0300

    feat: created login page

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Box from "@material-ui/core/Box";
+import useStyles from "./styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import RegisterForm from '../../components/Forms/Register/RegisterForm';
+import CardBox from '../../components/Card/CardBox';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import LoginForm from '../../components/Forms/Login/LoginForm';
+import Footer from '../../layouts/Footer/Footer';
+const LoginPage = () => {
+    const classes = useStyles();
+  return (
+    
+    <Box component="main" className={classes.content}>
+        <Grid container justifyContent='center' spacing={4} className={classes.gridContainer}>
+            <Grid item className={classes.card}>
+                <CardBox>
+                    <LoginForm />
+                </CardBox>
+                <CardBox>
+                    <Typography align='center' className={classes.typo}>
+                        Don't have an account? <Button component={Link} to="/register" color="primary">Sign up</Button>
+                    </Typography>
+                </CardBox>
+            </Grid>
+        </Grid>
+        <Footer />
+    </Box>
+  )
+}
+
+export default LoginPage
