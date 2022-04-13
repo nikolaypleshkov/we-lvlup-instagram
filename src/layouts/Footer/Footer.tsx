b7a5d1631e2eaf0bbf702d34df73fa6738206973
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-13 18:18:36 +0300

    feat: created the footer component

---
+++ src/layouts/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import useStyles from "./styles";
+import ListItem from '@material-ui/core/ListItem';
+import { Links as links } from './links';
+const Footer = (): JSX.Element => {
+    const classes = useStyles();
+  return (
+    <Box component="footer" className={classes.footer}>
+        <Box component="div" className={classes.ulContainer}>
+            <Box component="ul" className={classes.ul}>
+                {links.map((link, i) => (
+                    <ListItem key={i} className={classes.li}>
+                        <a href={link.path}>{link.name}</a>
+                    </ListItem>
+                ))}
+            </Box>
+        </Box>
+    </Box>
+  )
+}
+
+export default Footer
