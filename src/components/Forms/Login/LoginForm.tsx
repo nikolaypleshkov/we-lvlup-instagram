c436001c41b81f4c0672edca07ed07971023ab26
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-13 18:22:19 +0300

    feat: created the Login form

---
+++ src/components/Forms/Login/LoginForm.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Box, { BoxProps } from "@material-ui/core/Box";
+import Button from '@material-ui/core/Button'
+import FormControl from '@material-ui/core/FormControl'
+import TextField from '@material-ui/core/TextField'
+import FacebookIcon from "@material-ui/icons/Facebook";
+import { PrimaryButton } from '../../Buttons/Buttons';
+import useStyles from "./styles";
+import  logo  from "../../../assets/images/logo/logo.webp";
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  email: string;
+  password: string;
+}
+
+interface IBox{
+  src: BoxProps,
+  className: string,
+  alt: string
+}
+
+const BoxImage = (props: IBox) => <Box component="img" {...props} />
+
+const LoginForm = () => {
+  const classes = useStyles();
+  const { register, setValue, handleSubmit, formState: { errors }} = useForm<FormData>();
+
+  return (
+    <> 
+      <Box component="div" className={classes.cardLogoContainer}> 
+        <BoxImage src={logo} className={classes.cardLogo} alt="instagram-logo"/>
+      </Box>
+      <Box component="form" className={classes.form}>
+        <FormControl className={classes.form}>
+          <TextField size="small" id="outlined-size-small" variant="outlined" placeholder="Email" className={classes.textInput} {...register("email")} />
+          <TextField size="small" type="password" id="outlined-password-input" variant="outlined" placeholder="Password" className={classes.textInput} {...register("password")} />
+        </FormControl>
+        <FormControl className={classes.form}>
+          <PrimaryButton>Login</PrimaryButton>
+        </FormControl>
+        <FormControl className={classes.form}>
+          <div className="space-breaker">
+            <div className="line1"></div>
+            <div className="line1-text">or</div>
+            <div className="line1"></div>
+          </div>
+        </FormControl>
+        <FormControl className={classes.form}>
+          <Button color="primary"> <FacebookIcon />Log in with Facebook</Button>
+          <Button>Forgotten your password?</Button>
+        </FormControl>
+      </Box>
+    </>
+  )
+}
+
+export default LoginForm
