e073275994767d5f6270bea1c8782b8529b1f9aa
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-13 18:14:39 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./styles.scss";
 import "@fontsource/roboto"
-import Button from '@material-ui/core/Button';
-
+import AuthenticatePage from './pages/AuthenticatePage/AuthenticatePage';
+import HomePage from './pages/HomePage/HomePage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 type AppProps = {
   name: string
 }
 const App = (props: AppProps): JSX.Element => {
   return (
-      <div className="wrapper"> 
-        <h1>Instagram clone with</h1>
-        <h4>React 18 and TypeScript 4</h4>
-        <br />
-        <Button variant="outlined">Hello, {props.name}</Button>
-      </div>
+    <>
+      <Routes>
+        <Route path="/">
+          <Route index element={<AuthenticatePage />} />
+          <Route path="home" element={<HomePage />}/>
+          <Route path="register" element={<SignupPage />}/>
+          <Route path="login" element={<LoginPage />}/>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
