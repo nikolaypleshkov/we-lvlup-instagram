9849269e5dcc091df441759be155d6a88874a677
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-14 16:02:41 +0300

    style: changed the file formatting

--- src/layouts/TopBar/index.tsx
+++ src/layouts/TopBar/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react"
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 const TopBar = (): JSX.Element => {
   return (
     <Box sx={{ flexGrow: 1 }}>
