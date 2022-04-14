3d6da2abaed4995a4b62cb5ab22d9e87a8015330
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-14 16:01:00 +0300

    style: changed the file formatting

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Box from '@material-ui/core/Box'
+import React from "react"
+import Box from "@material-ui/core/Box"
 import useStyles from "./styles";
-import ListItem from '@material-ui/core/ListItem';
-import { Links as links } from './links';
+import ListItem from "@material-ui/core/ListItem";
+import { Links as links } from "./links";
 const Footer = (): JSX.Element => {
     const classes = useStyles();
   return (
