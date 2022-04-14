4b9adaaff11e1f9b3a4f88462397b1c0e18cd0fb
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-14 16:03:49 +0300

    style: changed the file formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles.scss";
 import "@fontsource/roboto"
-import AuthenticatePage from './pages/AuthenticatePage/AuthenticatePage';
-import HomePage from './pages/HomePage/HomePage';
-import SignupPage from './pages/SignupPage/SignupPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-type AppProps = {
-  name: string
-}
-const App = (props: AppProps): JSX.Element => {
+import AuthenticatePage from "./pages/AuthenticatePage/AuthenticatePage";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+const App = (): JSX.Element => {
   return (
     <>
       <Routes>
         <Route path="/">
           <Route index element={<AuthenticatePage />} />
           <Route path="home" element={<HomePage />}/>
-          <Route path="register" element={<SignupPage />}/>
+          <Route path="register" element={<RegisterPage />}/>
           <Route path="login" element={<LoginPage />}/>
         </Route>
       </Routes>
