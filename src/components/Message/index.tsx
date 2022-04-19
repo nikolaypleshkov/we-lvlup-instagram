345a25199fe7f3a81af67b4809b5322a974e83b5
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-19 11:40:49 +0300

    feat: message component

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent } from 'react'
+
+interface MessageInterface {
+    message: string;
+    type: "danger" | "success"
+}
+const Message: FunctionComponent<MessageInterface> = ({ message, type}) => {
+  let typeClass = "";
+  
+  if(type === "danger"){
+      typeClass = "error-message";
+  }
+  if(type === "success"){
+      typeClass = "success-message";
+  }
+  return (
+      <div className={typeClass}>
+          <span>{message}</span>
+      </div>
+  )
+}
+
+export default Message
