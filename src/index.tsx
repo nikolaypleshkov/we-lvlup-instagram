4fd6c4456975c101bb702030fd478c8322b5947d
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-19 11:41:19 +0300

    feat: store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import store from "redux/store";
+
+
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+        <React.StrictMode>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        </React.StrictMode>
 );
