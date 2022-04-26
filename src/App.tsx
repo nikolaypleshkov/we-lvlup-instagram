f800e722659d06df834f88f61bdd0b449a63e8b9
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-26 16:56:15 +0300

    feat: add public/private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import "./styles.scss";
 import "@fontsource/roboto"
 import AuthenticatePage from "./pages/AuthenticatePage/AuthenticatePage";
@@ -8,23 +8,65 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import { ThemeProvider } from "@mui/system";
 import { createTheme } from "@mui/material/styles";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "redux/store";
-
+import ProtectedRoute from "routes/ProtectedRoute";
+import PrivateRoutes from "routes/PrivateRoutes";
+import UserPage from "pages/UserPage/UserPage";
+import TopBar from "layouts/TopBar";
+import { logout } from "redux/actions/authActions";
+import UserSettingPage from "pages/UserSettingPage/UserSettingPage";
 
 const theme = createTheme();
 const App = (): JSX.Element => {
   const { authenticated } = useSelector((state: RootState) => state.auth);
+  const location = useLocation();
+  const dispatch = useDispatch();
+  console.log(location.pathname);
+  
+  useEffect(() => {
+    if(location.pathname === "/logout"){
+      dispatch(logout());
+    }
+  }, [location])
 
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/">
-          <Route index element={<AuthenticatePage />} />
-          <Route path="home" element={<HomePage />}/>
-          <Route path="register" element={<RegisterPage />}/>
-          <Route path="login" element={<LoginPage />}/>
+        <Route path="/" element={
+          <PrivateRoutes>
+            <AuthenticatePage />
+          </PrivateRoutes>
+        }>
         </Route>
+        <Route path="/login" element={
+          <PrivateRoutes>
+            <LoginPage />
+          </PrivateRoutes>
+        }>
+        </Route>
+        <Route path="/register" element={
+          <PrivateRoutes>
+            <RegisterPage />
+          </PrivateRoutes>
+        }>
+        </Route>
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        } />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <UserPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/logout" element={
+          <Navigate to="/" replace state={{ from: location }} />
+        } />
+        <Route path="/user-settings" element={
+          <UserSettingPage />
+        } />
       </Routes>
     </ThemeProvider>
   )
