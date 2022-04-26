294d50d0fbe6af2bd575c64a55c868cf6cc5033e
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-26 17:00:23 +0300

    feat: testing home page layout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import BottomNav from "layouts/BottomNavigation";
+import TopBar from "layouts/TopBar";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="main">
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+    </div>
+    );
 }
 
 export default HomePage;
