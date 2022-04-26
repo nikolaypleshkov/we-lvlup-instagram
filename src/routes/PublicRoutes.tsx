1d95662d489b256777795a18602a65ac05f449a3
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-26 16:57:31 +0300

    feat: created public route

---
+++ src/routes/PublicRoutes.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+const PublicRoutes = (path: string) => {
+  return (
+    <Route path={path}> 
+
+    </Route>
+  )
+}
+
+export default PublicRoutes
