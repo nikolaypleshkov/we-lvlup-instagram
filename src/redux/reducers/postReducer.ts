6c2b6f856eb2502d7df4bb67e6ace2e9af0a3321
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-29 18:35:38 +0300

    feat: created post reducer

---
+++ src/redux/reducers/postReducer.ts
@@ -0,0 +1,20 @@
+import { POST, PostState, UserPostAction } from "redux/types";
+
+const initState: PostState = {
+    user: null,
+    posts: null
+}
+
+export default (state = initState, action: UserPostAction) => {
+    switch(action.type){
+        case POST: {
+            return{
+                ...state,
+                posts: action.payload,
+                user: action.user
+            }
+        }
+        default: 
+            return state;
+    }
+}
