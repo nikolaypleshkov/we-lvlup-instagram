9f3dba8dbaeb39930a38e13e22ae82cffb478b11
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-05-05 10:27:18 +0300

    feat: like and dislike feature

--- src/redux/actions/postAction.ts
+++ src/redux/actions/postAction.ts
@@ -1,7 +1,7 @@
-import { query, collection, where, getDocs } from "firebase/firestore";
+import { query, collection, where, getDocs, doc, getDoc, updateDoc } from "firebase/firestore";
 import { ThunkAction } from "redux-thunk";
 import { RootState } from "redux/store";
-import { AuthAction, POST, Post, User, UserPostAction } from "redux/types";
+import { AuthAction, LIKE, POST, Post, User, UserPostAction } from "redux/types";
 import { db } from "service/firebaseSetup";
 
 export const userPosts = (user: User | null): ThunkAction<void, RootState, null, UserPostAction> => {
@@ -12,7 +12,6 @@ export const userPosts = (user: User | null): ThunkAction<void, RootState, null,
         querySnapshot.forEach((doc) => {
             posts.push(doc.data());
         });
-        console.log(posts);
         try{
             dispatch({
                 type: POST,
@@ -23,5 +22,81 @@ export const userPosts = (user: User | null): ThunkAction<void, RootState, null,
             console.log("Something went wron",err);
         }
         
+    }
+}
+
+export const postId = (id: string | null): ThunkAction<void, RootState, null, UserPostAction> => {
+    return async (dispatch) => {
+        const qry = query(collection(db, "posts"), where("createdByUserId", "==", id));
+        const querySnapshot = await getDocs(qry);
+        let posts: any[] = [];
+        querySnapshot.forEach((doc) => {
+            posts.push(doc.data());
+        });
+        console.log(posts);
+    }
+}
+
+export const likePost = (user: User, id: string | null): ThunkAction<void, RootState, null, UserPostAction> => {
+    return async(dispatch) => {
+        const docRef = doc(db, "posts", id!);
+        const docSnap = await getDoc(docRef);
+        let likesId: string[] = [];
+        let likesCount: number = 0;
+        if(docSnap.exists()){
+            likesId = docSnap.data().likesID;
+            likesId.push(user.uuid);
+            likesCount = likesId.length;
+        }
+        await updateDoc(docRef, {
+            likesID: likesId,
+            likes: likesCount
+        })
+        .then(async() => {
+            const qry = query(collection(db, "posts"), where("createdByUserId", "==", id));
+            const querySnapshot = await getDocs(qry);
+            let posts: any[] = [];
+            querySnapshot.forEach((doc) => {
+                posts.push(doc.data());
+            });
+            dispatch({
+                type: LIKE,
+                payload: posts,
+                user: user
+            })
+        })
+        console.log(likesId);
+        
+    }
+}
+
+export const dislikePost = (user: User, id: string | null): ThunkAction<void, RootState, null, UserPostAction> => {
+    return async(dispatch) => {
+        const docRef = doc(db, "posts", id!);
+        const docSnap = await getDoc(docRef);
+        let likesId: string[] = [];
+        let likesCount: number = 0;
+        if(docSnap.exists()){
+            likesId = docSnap.data().likesID;
+            likesId = likesId.filter((id) => id != user.uuid);
+            likesCount = likesId.length;
+        }
+        await updateDoc(docRef, {
+            likesID: likesId,
+            likes: likesCount
+        })
+        .then(async() => {
+            const qry = query(collection(db, "posts"), where("createdByUserId", "==", id));
+            const querySnapshot = await getDocs(qry);
+            let posts: any[] = [];
+            querySnapshot.forEach((doc) => {
+                posts.push(doc.data());
+            });
+            dispatch({
+                type: LIKE,
+                payload: posts,
+                user: user
+            })
+        })
     }
 }
